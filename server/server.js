ae166e9a39d98c0770b0f93278209515d6b660dd
jgjio <johngjackson0@gmail.com>
2021-11-10 06:35:22 -0500

    Apply cors to allow backend to run on a different port

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use("/api", authRoutes);
 //this is a console.log() er and it logs relevant req/res info
 app.use(morgan("dev"));
 
+//this is for development we run a different port on the react side
+//cors connects the two ports and makes them compatiable
+//apply everywhere, except we don't want it because on production
+//it is not needed because the "ports" will be domain instead
+//app.use(cors());
+
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
 const port = process.env.port || 8000;
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
